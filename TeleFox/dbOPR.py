c0a80573102f5a760c640326a3b4e43189578ce5
Yldzhn <70827987+Yldzhn@users.noreply.github.com>
2023-12-03 06:04:47 +0300

    Delete TeleFox directory

--- TeleFox/dbOPR.py
+++
@@ -1,9 +0,0 @@
-from tinydb import TinyDB, Query
-
-VT = TinyDB("requests.json")
-def inputSave (time,msg,lnk) :
-    VT.insert({"Timestamp":str(time),"Message":msg,"Link":lnk})
-def inputSearch(msg):
-    q=Query()
-    qMatch = VT.search(q.Message == msg)
-    return qMatch
